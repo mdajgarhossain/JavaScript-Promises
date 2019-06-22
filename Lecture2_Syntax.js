c08e8f8c01603f814b20ffe7c0973565dd624aa7
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-06-22 12:28:12 +0600

    More examples about Promise creation, Successful or Failing Response

--- Lecture2_Syntax.js
+++ Lecture2_Syntax.js
@@ -120,4 +120,152 @@ const myFifthPromise = new Promise((resolve,reject) => {
 myFifthPromise
     .then((courseName) => {
         console.log(courseName);
+    });
+
+
+//another example:::
+
+//Successful Request
+const mySixthPromise = new Promise((resolve, reject) => {
+    const frontend = new Set(['HTML', 'CSS', 'JavaScript', 'React', 'Redux']);
+    const backend = new Set(['JavaScript', 'MySQL', 'NodeJs']);
+
+    const developer = {
+        Front: {
+            name: 'Front-end Developer',
+            topics: frontend
+        },
+        Back: {
+            name: 'Back-end Developer',
+            topics: backend
+        },
+        Full: {
+            name: 'Fullstack Developer',
+        }
+    };
+    //garbage 
+    /*if(developer.Front.name === 'Front-end Developer') {
+        resolve(developer);
+    }
+     if(developer.Back.name === 'Back-end Developer') {
+        resolve(developer); //is many resolve() is working???
+    }
+     if(developer.Front.name === 'Front-end Developer' && developer.Back.name === 'Back-end Developer') {
+        resolve(developer);
+    }*/
+
+    if(developer.Front.name !== 'Front-end Developer' || developer.Back.name !== 'Back-end Developer' || developer.Full.name !== 'Fullstack Developer') {
+        const msg = 'Something went wrong, Please choose the correct developer!';
+        reject(msg);
+    }
+    resolve(developer);
+});
+
+mySixthPromise
+    .then((developer) => {
+        console.log(`First is ${developer.Front.name}, 
+        Second is ${developer.Back.name} and 
+            Third is ${developer.Full.name}.`);
+        console.log(`Front-end Topics: `);
+        console.log(developer.Front.topics);
     })
+    .catch((msg) => {
+        console.log(msg);
+    });
+
+//Failed Request /*for line 192 */
+const mySixthFailedPromise = new Promise((resolve, reject) => {
+    const frontend = new Set(['HTML', 'CSS', 'JavaScript', 'React', 'Redux']);
+    const backend = new Set(['JavaScript', 'MySQL', 'NodeJs']);
+
+    const developer = {
+        Front: {
+            name: 'Front-end Developer',
+            topics: frontend
+        },
+        Back: {
+            name: 'Back-end Developer',
+            topics: backend
+        },
+        Full: {
+            name: 'Fullstack Developer',
+        }
+    };
+
+    if(developer.Front.name !== 'Front-end Developer' || developer.Back.name !== 'Back-end Developer' || developer.Full.name !== 'FullstackDeveloper') {
+        const msg = 'Something went wrong, Please choose the correct developer!';
+        reject(msg);
+    }
+    resolve(developer);
+});
+
+mySixthFailedPromise
+    .then((developer) => {
+        console.log(`First is a ${developer.Front.name} & related topics ${developer.Front.topics}, 
+        Second is ${developer.Back.name} and 
+        Third is ${developer.Full.name}.`);
+    })
+    .catch((msg) => {
+        console.log(msg);
+    });
+
+
+//:::another example: Promises in ES6::
+
+//Successful request
+const mySeventhPromise = new Promise((resolve, reject) => {
+    const myName = 'Md. Ajgar Hossain Jewell';
+    if(myName !== 'Md. Ajgar Hossain Jewell') {
+        const errorMessage = 'Please enter my correct name.';
+        reject(errorMessage);
+    }
+    resolve(myName);
+});
+
+mySeventhPromise.then((myName) => {
+    console.log(`My Name is ${myName}`);
+}, (errorMessage) => {
+    console.log(errorMessage);
+    
+});
+
+//Failed request
+const mySeventhFailedPromise = new Promise((resolve, reject) => {
+    const myName = 'Md. Ajgar';
+    if(myName !== 'Md. Ajgar Hossain Jewell') {
+        const errorMessage = 'Enter the correct name.';
+        reject(errorMessage);
+    }
+    resolve(myName);
+});
+
+mySeventhFailedPromise.then((myName) => {
+    console.log(`My Name is ${myName}`);
+}, (errorMessage) => {
+    console.log(errorMessage);
+    
+});
+
+
+//:::another example with an asynchronous code:::
+const myEighthPromise = new Promise((resolve, reject) => {
+    const courseName = 'JavaScript Promises';
+    setTimeout(() => {
+        resolve(courseName);
+    }, 5000);
+});
+
+myEighthPromise.then((courseName) => {
+    console.log(`${courseName} is very important!`);
+});
+
+
+//:::::::::Lecture from Udacity:::::::::::::
+new Promise(function(resolve) {
+    console.log('First');
+    resolve();
+    console.log('Second');
+}).then(function() {
+    console.log('Third');
+});
+
